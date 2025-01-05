c98552d61d6aa69be4a60b75320e372d21bf8e3f
BugHunter-X <askzenai@gmail.com>
2025-01-05 03:20:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Example using Tailwind's @apply directive with a class that doesn't exist
+<div class="@apply non-existent-class">
+  This will cause a build error or unexpected behavior.
+</div>
+```
